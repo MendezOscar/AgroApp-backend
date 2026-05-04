607fe0a2206cc907279c7b19b4f0fb0eec6e527b
oscar.mendez <oscar.mendez@indimin.com>
2026-05-04 17:53:13 -0600

    feat: enable swagger in all environments

--- src/AgroApp.API/Program.cs
+++ src/AgroApp.API/Program.cs
@@ -68,11 +68,8 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
